f1309a570de38939d5282df7f4e075314223cbf7
Serotoninene <pujol.alexandre@hotmail.fr>
2024-01-08 17:05:33 +0100

    refactored by moving out the loader into the organisms

---
+++ src/components/organisms/Loader/index.tsx
@@ -0,0 +1,32 @@
+import { useProgress } from "@react-three/drei";
+import { useRef, useLayoutEffect, RefObject } from "react";
+
+export const Loader = () => {
+  const ref = useRef<HTMLElement>();
+  const container = useRef<HTMLDivElement>();
+  const tl = useRef<gsap.core.Timeline>();
+  const { progress } = useProgress();
+
+  useLayoutEffect(() => {
+    if (!ref.current || !container.current) return;
+    tl.current = gsap.timeline();
+    tl.current.to(ref.current, {
+      scaleX: progress / 100,
+    });
+
+    if (progress === 100) gsap.to(container.current, { yPercent: -100 });
+  }, [progress]);
+
+  return (
+    <div
+      ref={container as RefObject<HTMLDivElement>}
+      className="fixed top-0 left-0 w-screen h-screen bg-secondary-200 z-50 flex flex-col justify-center items-center"
+    >
+      <div
+        ref={ref as RefObject<HTMLDivElement>}
+        className="h-[1px] w-[50vw] origin-left scale-x-0 bg-dark"
+      />
+      <div>{progress}</div>
+    </div>
+  );
+};
