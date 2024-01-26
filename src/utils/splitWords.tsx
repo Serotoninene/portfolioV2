324a90e329f0f36ff1f9bb916dc35c4954dfd5c0
Serotoninene <pujol.alexandre@hotmail.fr>
2024-01-26 16:04:16 +0100

    cleaned the bug with p and span

--- src/utils/splitWords.tsx
+++ src/utils/splitWords.tsx
@@ -12,12 +12,12 @@ export const splitWords = (
     const letters = splitLetters(word, ref);
     body.push(
       <Fragment key={word + i}>
-        <p
+        <span
           ref={(e) => ref?.current?.push(e)}
           className={`inline-block overflow-${overflow}`}
         >
           {letters}
-        </p>{" "}
+        </span>{" "}
       </Fragment>
     );
   });
