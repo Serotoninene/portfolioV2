2c8dba003f13dc339c614bacceadc78463e446b1
Serotoninene <pujol.alexandre@hotmail.fr>
2024-02-02 11:11:02 +0100

    no AlexReveal when  not on homepage

--- src/components/molecules/Navbar/animations/useAlexReveal.ts
+++ src/components/molecules/Navbar/animations/useAlexReveal.ts
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { useLocation } from "react-router-dom";
 
 export const useAlexReveal = () => {
   const [hasScrolled, setHasScrolled] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
+    if (location.pathname !== "/") {
+      setHasScrolled(true);
+      return;
+    }
+
+    setHasScrolled(false);
     ScrollTrigger.create({
       start: "top top", // Trigger after scrolling 100vh
       end: `${window.innerHeight}px top`,
       onLeave: () => setHasScrolled(true),
-      markers: true,
       onEnterBack: () => setHasScrolled(false),
     });
-  }, []);
+  }, [location]);
   return hasScrolled;
 };
